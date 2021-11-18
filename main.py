f7d46b1a1ad864288f57548266e3d95c173f8bb9
Jose Edmar de Siqueira <jesiqueira@bbmapfre.com.br>
2021-11-18 17:18:24 -0300

    Modificado conexao com binance

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from binance.spot import Spot as Client
+from binance.client import Client
+from time import sleep
+from binance import ThreadedDepthCacheManager
 from config import Config
+import pprint
 
-client = Client(base_url='https://testnet.binance.vision', key=Config.API_KEY, secret=Config.API_SECRET)
-print(client.time())
 
-# client = Spot(key=Config.API_KEY, secret=Config.API_SECRET)
+client = Client(Config.API_KEY, Config.API_SECRET)
+client.API_URL = 'https://testnet.binance.vision/api'
 
-# Get account information
-print(client.account())
+# Informações da conta
+# print(client.get_account())
+
+# Preço mais recente do Bitcoin
+btc_price = client.get_symbol_ticker(symbol='BTCUSDT')
+# print(btc_price)
+
+def btc_trade_history(msg):
+    '''definir como processar mensagens WebSocket recebidas '''
+
+    if msg['e'] != 'error':
+        print(msg['c'])
+        btc_price['last'] = msg['c']
+        btc_price['bid'] = msg['b']
+        btc_price['ask'] = msg['a']
+        btc_price['error'] = False
+    else:
+        btc_price['error'] = True
+
+bsm = ThreadedDepthCacheManager()
+bsm.start()
+
+# subscribe to a stream
+bsm.start_symbol_ticker_socket(callback=btc_trade_history, symbol='BTCUSDT')
+
+# stop websocket
